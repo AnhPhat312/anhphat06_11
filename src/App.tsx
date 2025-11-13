80de45f43b7e483eaabaf35689478573583c6611
anhphat312 <anhphatnguyen.312@gmail.com>
2025-11-13 04:15:05 +0000

    Initial commit of the React project

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,21 @@ import Layout from "./Layout";
 import Trang1 from "./Trang1";
 // @ts-ignore
 import Chitietsanpham from "./Chitietsanpham";
+
 // @ts-ignore
-import Trang2 from "./Trang2";
+import ProductDetail from "./ProductDetail";
+
 // @ts-ignore
 import Listproduct from "./Listproduct";
 
 // @ts-ignore
 import ListProducts_SP from "./ListProducts_SP";
+
 // @ts-ignore
-import ProductDetail from "./ProductDetail";
+import Trang2 from "./Trang2";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 //@ts-ignore
 import LoginPage from "./LoginPage";
 //@ts-ignore
@@ -26,19 +30,26 @@ import LogoutPage from "./LogoutPage";
 import ProtectedRoute from "./ProtectedRoute";
 //@ts-ignore
 import ListProducts_SP_Admin from "./ListProducts_SP_Admin";
+//@ts-ignore
+import EditProduct from "./EditProduct";
 
-const App = () => {
-  //return <Layout />;
+export default function App() {
+  // return <Layout />;
+
   return (
     <BrowserRouter>
       <Routes>
+        {/* ✅ Layout chung cho toàn bộ hệ thống */}
         <Route path="/" element={<Layout />}>
+          {/* Trang chính (cho người dùng vãng lai) */}
           <Route index element={<ListProducts_SP />} />
           <Route path="trang1" element={<Trang1 />} />
-          <Route path="sanpham/:id" element={<Chitietsanpham />} />
           <Route path="trang2" element={<Trang2 />} />
+          <Route path="sanpham/:id" element={<Chitietsanpham />} />
+          <Route path="/admin/edit/:id" element={<EditProduct />} />
           <Route path="Listproduct" element={<Listproduct />} />
           <Route path="detail/:id" element={<ProductDetail />} />
+
           {/* <Route path="detail/:id" element={<ProductDetail />} /> */}
 
           {/* ✅ Trang đăng nhập (nằm trong Layout) */}
@@ -46,19 +57,18 @@ const App = () => {
 
           {/* ✅ Trang đăng xuất */}
           <Route path="logout" element={<LogoutPage />} />
+
+          {/* ✅ Trang quản trị (nằm trong Layout, chỉ Admin truy cập) */}
+          <Route
+            path="admin/products"
+            element={
+              <ProtectedRoute>
+                <ListProducts_SP_Admin />
+              </ProtectedRoute>
+            }
+          />
         </Route>
-        {/* ✅ Trang quản trị (nằm trong Layout, chỉ Admin truy cập) */}
-        <Route
-          path="admin/products"
-          element={
-            <ProtectedRoute>
-              <ListProducts_SP_Admin />
-            </ProtectedRoute>
-          }
-        />
       </Routes>
     </BrowserRouter>
   );
-};
-
-export default App;
+}
